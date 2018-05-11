e39fc8fe941c10cb71789afcaeba1a8c6e1d3048
yellowskt <guilhermedesouzalucas@gmail.com>
2018-05-11 09:05:47 -0300

    resolvi o conflitos de olas

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
-<<<<<<< HEAD
-	ehco "Olá Mundo!456";
 
-	echo "Olá Yellow";
-	
-=======
-	ehco "Olá Mundo!123";
+	echo "Olá Mundo!123";
 
 	echo "Olá Guilherme";
->>>>>>> 0878d471b4a1bf8d4a0858fbc84d25fe4f38e638
+
 ?>
